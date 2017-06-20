380054140bcebb3a380c5a6adad146d6f7001c8a
Francis Lee <francis.g.lee@gmail.com>
2017-06-20 08:35:37 -0400

    Update gs_kernel_slow.py

--- antimicrobial-peptide-design/gs_kernel/gs_kernel_slow.py
+++ antimicrobial-peptide-design/gs_kernel/gs_kernel_slow.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 '''
----------------------------------------------------------------------
-Copyright 2011, 2012, 2013 Sébastien Giguère
-
-This file is part of GSkernel
-
-GSkernel is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-GSkernel is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GSkernel.  If not, see <http://www.gnu.org/licenses/>.
----------------------------------------------------------------------
-
 This module will compute a square positive semi-define gram matrix or a
 possibly rectangle matrix (for testing examples) using the GS kernel.
 
